d474743cada952cc2c6f8e9a72a7028d5fbaca6a
Ahmet AYGÜN <me@ahmetaygun.net>
2012-06-10 09:26:03 +0300

    add gen_doc scrpt

---
+++ generate_docs.sh
@@ -0,0 +1,1 @@
+appledoc --verbose 1 --ignore .m -o ./appledoc -p "PutioKit" -v "0.0.1" -c "Ahmet AYGUN" --company-id "com.ahmetaygun" --keep-intermediate-files --keep-undocumented-objects --keep-undocumented-members --docsetutil-path /Applications/Xcode.app/Contents/Developer/usr/bin/docsetutil PutioKit
