b761e60260e23dba8752dc899edecbab64b3cea2
Ahmet AYGÜN <me@ahmetaygun.net>
2012-09-08 00:32:59 +0300

    update generate_appledoc script

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,1 +1,3 @@
-appledoc --verbose 1 --ignore .m -o ./appledoc -p "PutioKit" -v "0.0.1" -c "Ahmet AYGUN" --company-id "com.ahmetaygun" --keep-intermediate-files --keep-undocumented-objects --keep-undocumented-members --docsetutil-path /Applications/Xcode.app/Contents/Developer/usr/bin/docsetutil PutioKit
+#!/bin/sh
+
+/usr/local/bin/appledoc --verbose xcode --exit-threshold 2 --ignore .m -o ./appledoc -p "PutioKit" -v "0.0.1" -c "Ahmet AYGUN" --company-id "com.ahmetaygun" --keep-intermediate-files --keep-undocumented-objects --keep-undocumented-members --docsetutil-path /Applications/Xcode.app/Contents/Developer/usr/bin/docsetutil PutioKit
