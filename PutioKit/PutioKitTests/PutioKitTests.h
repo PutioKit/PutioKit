6c5a088c5c04a1651dc721ad9dad949ee3721df9
Ahmet AYGÜN <me@ahmetaygun.net>
2012-09-07 22:33:47 +0300

    change to cocoa touch static library project

---
+++ PutioKit/PutioKitTests/PutioKitTests.h
@@ -0,0 +1,13 @@
+//
+//  PutioKitTests.h
+//  PutioKitTests
+//
+//  Created by Ahmet AYGÜN on 07.09.2012.
+//  Copyright (c) 2012 Ahmet AYGÜN. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PutioKitTests : SenTestCase
+
+@end
---
+++ PutioKit/PutioKitTests/PutioKitTests.m
@@ -0,0 +1,32 @@
+//
+//  PutioKitTests.m
+//  PutioKitTests
+//
+//  Created by Ahmet AYGÜN on 07.09.2012.
+//  Copyright (c) 2012 Ahmet AYGÜN. All rights reserved.
+//
+
+#import "PutioKitTests.h"
+
+@implementation PutioKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PutioKitTests");
+}
+
+@end
